c63e35f8ddceb6b4f118d27dd16a4a6444bfcccf
afd <afd@alup5.academiapostal.es.local>
2020-01-24 17:56:04 +0100

    cambios index 1

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
-import { toRoman } from './index';
+import { calcularTotal } from './index.js';
 import { equal } from "assert";
 
 describe(('Cálculo del total'), () => {
   it('carrito vacío', () => { equal(calcularTotal([]), 0) })
+  it('carrito vacío', () => { equal(calcularTotal([ 
+    {producto:"pan", precio:1 }
+  ]),1) });
+ 
 })
