3b35f69cbbc8aa8f443f6424d10a7a41b8894d5f
afd <afd@alup5.academiapostal.es.local>
2020-01-24 18:33:14 +0100

    cambios nuevos carrito 2

--- index.js
+++ index.js
@@ -8,7 +8,13 @@
 export function calcularTotal(carrito) {
   var total= 0;
   for(var i=0;i<carrito.length;i++){
+    if(carrito[i].cantidad){ 
+    total+=carrito[i].precio*carrito[i].cantidad;
+  }
+  else
+  {
     total+=carrito[i].precio;
+  }
   }
   
   return total;
--- test.js
+++ test.js
@@ -3,8 +3,18 @@ import { equal } from "assert";
 
 describe(('Cálculo del total'), () => {
   it('carrito vacío', () => { equal(calcularTotal([]), 0) })
-  it('carrito vacío', () => { equal(calcularTotal([ 
+  it('pan', () => { equal(calcularTotal([ 
     {producto:"pan", precio:1 }
   ]),1) });
+  
+  it('pan y leche', () => { equal(calcularTotal([ 
+    {producto:"pan", precio:1 },
+    {producto: "leche", precio: 1}
+  ]),2) });
+
+  it('comprar huevos', () => { equal(calcularTotal([ 
+    {producto:"huevo", precio:30, cantidad:12 }
+    
+  ]),360) });
  
 })
